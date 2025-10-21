4a9db4591b11e68c2b845b37c13526028dcc5e52
Levyyyy <Levi.moreau@edu.sait.ca>
2025-10-20 23:13:55 -0600

    Started benchmarking with only test arrays and available sort algorithms in AppDriver

--- assignment1StartingCode/src/appDomain/AppDriver.java
+++ assignment1StartingCode/src/appDomain/AppDriver.java
@@ -1,4 +1,6 @@
 package appDomain;
+import utilities.BubbleSort;
+import utilities.SelectionSort;
 
 /**
  * <p>
@@ -26,7 +28,57 @@ public class AppDriver
 		// line arguments and benchmarking tests
 		
 		
-
+		// Bubble Sort Benchmarking (Replace test array with array from file read parse)
+		long start, stop;
+		
+		start = System.currentTimeMillis();
+        BubbleSort sortArray = new BubbleSort();
+        
+        double[] testArray = {15.445, 8.234, 5.679, 5.78, 11.3};
+        System.out.println("Original Array: ");
+        
+        for (double val : testArray) {
+            System.out.println(val);
+        }
+        
+        System.out.println();
+        
+        System.out.println("Sorted Array: ");
+        sortArray.sort(testArray);
+        
+        for (double numbers : testArray) {
+            System.out.println(numbers);
+        }
+        
+		stop = System.currentTimeMillis();
+		System.out.println( "Alotted time for Bubble Sort was: " + ( stop - start ) + " milliseconds" );
+		System.out.println();
+		
+		
+		// Selection Sort Benchmarking (Replace test array with array from file read parse)
+		start = System.currentTimeMillis();
+        SelectionSort newArray = new SelectionSort();
+        
+        double[] testArray2 = {15.445, 8.234, 5.679, 5.78, 11.3, 8.234};
+        System.out.println("Original Array: ");
+        
+        for (double val : testArray2) {
+            System.out.println(val);
+        }
+        
+        System.out.println();
+        
+        System.out.println("Sorted Array: ");
+        newArray.sort(testArray2);
+        
+        for (double numbers : testArray2) {
+            System.out.println(numbers);
+        }
+        
+        stop = System.currentTimeMillis();
+		System.out.println( "Alotted time for Selection Sort was: " + ( stop - start ) + " milliseconds" );
+		System.out.println();
+        
 		// refer to demo02 Student.java for comparable implementation, and
 		// NameCompare.java or GradeCompare for comparator implementations
 
